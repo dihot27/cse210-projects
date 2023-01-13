e4b396eaea7db2d961a78e35e77c7621c63634d5
dihot27 <dihot@gmail.co.za>
2023-01-13 11:19:53 +0200

    added file

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+       
     {
-        Console.WriteLine("Hello Prep3 World!");
+         String val;
+         
+        Console.WriteLine("What is the magic number?");
+        val =Console.ReadLine();
+        Console.WriteLine("What is your guess?");
+        val=Console.ReadLine();
+        
     }
 }
