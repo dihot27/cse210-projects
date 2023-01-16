5750a486bbc4521e6e2abc6cdc633e290d156a65
dihot27 <dihot@gmail.co.za>
2023-01-16 21:41:27 +0200

    added

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        int i,n,sum=0;
+        Console.Write("Enter your numbers");
+        for(i=1;i<=8;i++)
+        {
+            Console.Write("Number-{0}:",i);
+            n=Convert.ToInt32(Console.ReadLine());
+            sum+=n;
+        }
+
     }
 }
