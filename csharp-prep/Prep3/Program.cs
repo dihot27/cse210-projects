73d3a5eb05f47ea371948f46d16f83cdb0263582
dihot27 <dihot@gmail.co.za>
2023-01-16 20:17:59 +0200

    added code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,38 +3,37 @@ using System;
 class Program
 {
     static void Main(string[] args)
-
-       
     {
-
-         
-        Random randomGenerator= new Random();
-        int number= randomGenerator.Next(1,100);
-        int magic=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("What is your guess?");
-        int guess=Convert.ToInt32(Console.ReadLine());
-        while(magic!=guess)
         
-        {
-            Console.WriteLine("What is your guess?");
-            
-        }
-
-        if(magic>guess)
+        Console.Write("What is the magic number? ");
+         int magicNumber = int.Parse(Console.ReadLine());
         
-        {
-            Console.WriteLine("Higher");
-
-        }
-        if(magic<guess)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if(magic==guess)
-        {
-            Console.WriteLine("You guessed it!");
-        }
         
-}
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
+    }
 }
 
